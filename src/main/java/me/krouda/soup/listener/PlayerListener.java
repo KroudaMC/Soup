e1a7f2b0fc25c25e520bc8b460d14bdf0c77881e
Krouda <Krouda@gmail.com>
2022-09-08 21:43:18 +0900

    what the fuck

--- src/main/java/me/krouda/soup/listener/PlayerListener.java
+++ src/main/java/me/krouda/soup/listener/PlayerListener.java
@@ -1,5 +1,6 @@
 package me.krouda.soup.listener;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,10 +14,14 @@ public class PlayerListener implements Listener {
     public void useSoup(PlayerInteractEvent sex) {
         Player player = sex.getPlayer();
         Action action = sex.getAction();
-        if (action.equals("RIGHT_")) {
+        if (action.name().startsWith("RIGHT_") && player.getGameMode() == GameMode.SURVIVAL) {
             if (player.getItemInHand().getType() == Material.MUSHROOM_SOUP) {
-                if (player.getHealth() < 20) {
-                    player.setHealth(player.getHealth() + 2.5);
+                if (player.getHealth() <= 19.0D && !player.isDead()) {
+                    if (player.getHealth() < 20.0D || player.getFoodLevel() < 20)
+                        player.getItemInHand().setType(Material.BOWL);
+                    player.setHealth(Math.min(player.getHealth() + 7.0D, 20.0D));
+                    player.setFoodLevel(Math.min(player.getFoodLevel() + 2, 20));
+                    player.updateInventory();
                 }
             }
         }
