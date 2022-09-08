b506a4a07cc626a3cd1e071ec12352c61e684349
Krouda <Krouda@gmail.com>
2022-09-08 21:47:00 +0900

    added soup refill sign yesyes

--- src/main/java/me/krouda/soup/listener/PlayerListener.java
+++ src/main/java/me/krouda/soup/listener/PlayerListener.java
@@ -1,12 +1,19 @@
 package me.krouda.soup.listener;
 
+import me.krouda.soup.Soup;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerListener implements Listener {
 
@@ -23,6 +30,21 @@ public class PlayerListener implements Listener {
                     player.setFoodLevel(Math.min(player.getFoodLevel() + 2, 20));
                     player.updateInventory();
                 }
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onClickSign(PlayerInteractEvent dog) {
+        if (dog.getClickedBlock().getType().name().contains("SIGN") && dog
+                .getClickedBlock().getState() instanceof Sign) {
+            Sign sign = (Sign)dog.getClickedBlock().getState();
+            if (ChatColor.stripColor(sign.getLine(1)).equals("[Soup]")) {
+                dog.setCancelled(true);
+                Inventory inventory = Soup.getInstance().getServer().createInventory(null, 54, ChatColor.GRAY + "Soup Refill");
+                for (int i = 0; i < 54; i++)
+                    inventory.setItem(i, new ItemStack(Material.MUSHROOM_SOUP));
+                dog.getPlayer().openInventory(inventory);
             }
         }
     }
