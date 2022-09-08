fc624f2552b417cd8777bcfa6e4e59d2ddecc088
Krouda <Krouda@gmail.com>
2022-09-08 21:59:51 +0900

    add cooltimes in soup refill sign

--- src/main/java/me/krouda/soup/listener/PlayerListener.java
+++ src/main/java/me/krouda/soup/listener/PlayerListener.java
@@ -14,8 +14,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class PlayerListener implements Listener {
+
+    private final Map<UUID, Integer> cooldown = new HashMap<>();
+    public static final int COOLDOWN = 30;
 
     @EventHandler
     public void useSoup(PlayerInteractEvent sex) {
@@ -36,16 +44,48 @@ public class PlayerListener implements Listener {
     
     @EventHandler
     public void onClickSign(PlayerInteractEvent dog) {
-        if (dog.getClickedBlock().getType().name().contains("SIGN") && dog
-                .getClickedBlock().getState() instanceof Sign) {
-            Sign sign = (Sign)dog.getClickedBlock().getState();
-            if (ChatColor.stripColor(sign.getLine(1)).equals("[Soup]")) {
-                dog.setCancelled(true);
-                Inventory inventory = Soup.getInstance().getServer().createInventory(null, 54, ChatColor.GRAY + "Soup Refill");
-                for (int i = 0; i < 54; i++)
-                    inventory.setItem(i, new ItemStack(Material.MUSHROOM_SOUP));
-                dog.getPlayer().openInventory(inventory);
+        Player player = dog.getPlayer();
+        int coolDownLeft = getCoolDown(player.getUniqueId());
+        if (dog.getClickedBlock() == null) {
+            if (dog.getClickedBlock().getType().name().contains("SIGN") && dog
+                    .getClickedBlock().getState() instanceof Sign) {
+                Sign sign = (Sign) dog.getClickedBlock().getState();
+                if (ChatColor.stripColor(sign.getLine(1)).equals("[Soup]")) {
+                    if (coolDownLeft == 0) {
+                        dog.setCancelled(true);
+                        Inventory inventory = Soup.getInstance().getServer().createInventory(null, 54, ChatColor.GRAY + "Soup Refill");
+                        for (int i = 0; i < 54; i++)
+                            inventory.setItem(i, new ItemStack(Material.MUSHROOM_SOUP));
+                        player.openInventory(inventory);
+
+                        setCoolDown(player.getUniqueId(), COOLDOWN);
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                int timeLeft = getCoolDown(player.getUniqueId());
+                                setCoolDown(player.getUniqueId(), --timeLeft);
+                                if (timeLeft == 0) {
+                                    this.cancel();
+                                }
+                            }
+                        }.runTaskTimer(Soup.getInstance(), 20, 20);
+                    } else {
+                        player.sendMessage(ChatColor.RED.toString() + coolDownLeft + " seconds left to use again.");
+                    }
+                }
             }
         }
+    }
+
+    public void setCoolDown(UUID player, int time){
+        if(time < 1) {
+            cooldown.remove(player);
+        } else {
+            cooldown.put(player, time);
+        }
+    }
+
+    public int getCoolDown(UUID player){
+        return cooldown.getOrDefault(player, 0);
     }
 }
